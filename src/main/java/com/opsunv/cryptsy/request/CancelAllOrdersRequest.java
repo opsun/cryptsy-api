161b11bdc10c88e446cf0f6ac56142fe6fa35574
opsun <cerz@qq.com>
2014-06-07 20:49:35 +0800

    implementation cancelallorders api

--- src/main/java/com/opsunv/cryptsy/request/CancelAllOrdersRequest.java
+++ src/main/java/com/opsunv/cryptsy/request/CancelAllOrdersRequest.java
@@ -1,5 +1,24 @@
 package com.opsunv.cryptsy.request;
 
-public class CancelAllOrdersRequest {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.opsunv.cryptsy.CryptsyRequest;
+
+/**
+ * Authenticated API <b>cancelallorders</b><br>
+ * For more detail: <a href="https://www.cryptsy.com/pages/privateapi">https://www.cryptsy.com/pages/privateapi</a>
+ * @author opsun
+ * @since 1.0, 2014-6-7
+ */
+public class CancelAllOrdersRequest extends CryptsyRequest<Boolean> {
+
+	@Override
+	public String getMethod() {
+		return "cancelallorders";
+	}
+	
+	@Override
+	public Boolean parse(JsonNode json) {
+		return true;
+	}
 
 }
